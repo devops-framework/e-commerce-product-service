87af6d41cff49d3cd28542efe0170e36eb96ce53
Trần Thái Minh Tân <tanlun04112000@gmail.com>
2026-03-29 17:57:43 +0700

    Add deps to 3rd common

--- src/main/java/com/homelab/product/RestConfig.java
+++ src/main/java/com/homelab/product/RestConfig.java
@@ -9,12 +9,4 @@ import java.util.Set;
 
 @ApplicationPath("/api")
 public class RestConfig extends Application {
-	@Override
-	public Set<Class<?>> getClasses() {
-		Set<Class<?>> classes = new HashSet<>();
-		// Explicitly register resource classes so they are available even if
-		// annotation scanning is not enabled in the container.
-		classes.add(ProductResource.class);
-		return classes;
-	}
 }
